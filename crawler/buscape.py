fdbc9ee589dae07f5da6259dab3846e7c5838099
renanidc <renanidc@gmail.com>
2017-11-01 15:06:30 -0300

    Atualização da base de produto

--- crawler/buscape.py
+++ crawler/buscape.py
@@ -2,6 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import time
 import csv
 
 itens = []
@@ -32,13 +33,62 @@ def capturarProdutos():
 
 		try:
 			r = requests.get('http://www.buscape.com.br'+produto)
+
 			soup = BeautifulSoup (r.content, 'lxml')
 
+			#Encontrar produtos com js-discount
 			for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns js-discount'}):
-				print produto.find('a')['href']
+				print 'PRIMEIRA PAGINA DA CATEGORIA: ', produto.find('a')['href']
 				arquivo.write(produto.find('a')['href']+";"+categoria+'\n')
-		except:
-			print 'Link invalido!'
 
-#capturarItensMenu()
-#capturarProdutos()
+			#Encontrar produtos com prioridade first
+			for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns first'}):
+				print 'PRODUTO COM FIRST: ', produto.find('a')['href']
+				arquivo.write(produto.find('a')['href']+";"+categoria+'\n')					
+
+			#Encontrar produtos sem js-discount	
+			for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns'}):
+				print 'PRODUTO SEM JS-DISCOUNT: ', produto.find('a')['href']
+				arquivo.write(produto.find('a')['href']+";"+categoria+'\n')
+
+
+			old_link = None #Gambiarra :)
+
+			while(soup.find('li',attrs={'class':'bui-pagination--item next active'})!=None): #Navegar na paginação dos produtos
+
+				link = soup.find('li',attrs={'class':'bui-pagination--item next active'})
+				link = link.find('a')['href'] #Encontrar link da próxima página
+				print 'PROXIMA PAGINA DE PRODUTOS: ', link
+
+				#Evitar loop que fica repetindo o mesmo link (Bug quântico) '-'
+				if(old_link!=None and old_link==link):
+					break
+
+				r = requests.get(link)
+
+				soup = BeautifulSoup (r.content, 'lxml')
+
+				#Encontar produtos com js-discount
+				for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns js-discount'}):
+					print 'PRODUTO COM JS-DISCOUNT: ', produto.find('a')['href']
+					arquivo.write(produto.find('a')['href']+";"+categoria+'\n')
+
+				#Encontrar produtos com prioridade first
+				for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns first'}):
+					print 'PRODUTO COM FIRST: ', produto.find('a')['href']
+					arquivo.write(produto.find('a')['href']+";"+categoria+'\n')					
+
+				#Encontrar produtos sem js-discount	
+				for produto in soup.findAll('li', attrs={'class':'bui-product__container small-12 medium-4 large-4 columns'}):
+					print 'PRODUTO SEM JS-DISCOUNT: ', produto.find('a')['href']
+					arquivo.write(produto.find('a')['href']+";"+categoria+'\n')
+
+				old_link = link #Receber link antigo
+
+		except:
+			print 'Pagina fora do formato padrao!'
+		finally:
+			time.sleep(1)
+
+capturarItensMenu()
+capturarProdutos()
